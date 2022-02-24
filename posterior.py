da53cfdf5238cf07664d37e5254c2b15f4c7bec8
tadamcz <tmkadamcz@gmail.com>
2022-02-24 14:52:01 +0000

    refactor web app into its own repo
    Web app will live in: https://github.com/tadamcz/bayes-webapp

--- posterior.py
+++ posterior.py
@@ -1,13 +1,8 @@
-import math
-import matplotlib.pyplot as plt
 import numpy as np
 from scipy import stats
 from scipy import integrate
 from scipy import optimize
 import time
-import decimal
-import json
-import mpld3
 
 class Posterior(stats.rv_continuous): # todo docstrings
 	def __init__(self, distribution1, distribution2, user_inputs):
@@ -151,40 +146,6 @@ class Posterior(stats.rv_continuous): # todo docstrings
 
 		return {'result': sorted_result, 'runtime': description_string}
 
-	def graph_out(self):
-		plt.rcParams.update({'font.size': 16})
-		override_graph_range = self.user_inputs['override_graph_range']
-
-		# Plot
-		if override_graph_range:
-			x_from, x_to = override_graph_range
-		else:
-			x_from, x_to = intelligently_set_graph_domain(self.distribution1, self.distribution2)
-
-		plot = plot_pdfs_bayes_update(self.distribution1, self.distribution2, self, x_from=x_from, x_to=x_to)
-		plot = mpld3.fig_to_html(plot)
-
-		return plot
-
-	def distribution_information_out(self):
-		# expected value
-		ev = self.expect(epsrel=1 / 100)  # epsrel is the relative tolerance passed to the integration routine
-		ev_string = '<br>Expected value: ' + str(np.around(ev, 2)) + '<br>'
-
-		# percentiles
-		percentiles_exact_string = 'Percentiles:<br>'  # todo use a list instead of line breaks
-
-		if self.user_inputs['custom_percentiles']:
-			p = self.user_inputs['custom_percentiles']
-		else:
-			p = [0.1, 0.25, 0.5, 0.75, 0.9]
-		percentiles_exact = self.compute_percentiles(p)
-
-		for x in percentiles_exact['result']:
-			percentiles_exact_string += str(x) + ', ' + str(np.around(percentiles_exact['result'][x], 2)) + '<br>'
-		percentiles_exact_string += percentiles_exact['runtime']
-		return ev_string + percentiles_exact_string
-
 class CustomFromPDF(stats.rv_continuous):
 	def __init__(self, pdf_callable,a=-np.inf,b=np.inf):
 		super(CustomFromPDF, self).__init__()
@@ -259,49 +220,6 @@ def split_integral(function_to_integrate,splitpoint,integrate_to,support=(-np.in
 		integral_left = integrate.quad(function_to_integrate, support_left, splitpoint)[0]
 		integral_right = integrate.quad(function_to_integrate, splitpoint, integrate_to)[0]
 		return integral_left + integral_right
-
-def plot_pdfs(dict_of_dists,x_from,x_to):
-	x_from ,x_to = float(x_from),float(x_to)
-	x = np.linspace(x_from,x_to,100)
-
-	figure, axes = plt.subplots()
-	for dist in dict_of_dists:
-		axes.plot(x,dict_of_dists[dist].pdf(x),label=dist)
-	axes.legend()
-	axes.set_xlabel("θ")
-	axes.set_ylabel("Probability density")
-	return figure
-
-def plot_pdfs_bayes_update(prior,likelihood,posterior,x_from=-50,x_to=50):
-	prior_string = "f₀(θ) = P(θ)"
-	likelihood_string = "f₁(θ) = P(E|θ)"
-	posterior_string = "P(θ|E)"
-
-	plot = plot_pdfs({prior_string:prior, likelihood_string:likelihood, posterior_string:posterior},
-					x_from,
-					x_to)
-	return plot
-
-def intelligently_set_graph_domain(prior,likelihood):
-	p = 0.1
-	try:
-		prior_range = np.quantile(prior.monte_carlo_samples,(p,1-p))
-	except AttributeError:
-		prior_range = prior.ppf(p), prior.ppf(1-p)
-
-	likelihood_range = likelihood.ppf(p), likelihood.ppf(1-p)
-
-	ranges = extremeties_intervals([prior_range,likelihood_range])
-
-	posterior_support = intersect_intervals([prior.support(),likelihood.support()])
-
-	domain = intersect_intervals([posterior_support,ranges])
-
-	buffer = 0.1
-	buffer = abs(buffer*(domain[1]-domain[0]))
-	domain = domain[0]-buffer,domain[1]+buffer
-
-	return domain
 
 def normal_parameters(x1, p1, x2, p2):
 	"Find parameters for a normal random variable X so that P(X < x1) = p1 and P(X < x2) = p2."
